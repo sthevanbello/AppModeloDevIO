d4716d21c9037d7a06625ca3b1d7a0ea4cd150d7
sthevanbello <sthevan.alves@gmail.com>
2023-04-25 08:29:31 -0300

    Add project files.

---
+++ src/DevIO.UI.Site/Extensions/EmailTagHelper.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace DevIO.UI.Site.Extensions
+{
+    public class EmailTagHelper : TagHelper
+    {
+        public async override Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+            var content = await output.GetChildContentAsync();
+            var target = content.GetContent() + "@" + "gmail.com";
+            output.Attributes.SetAttribute("href", "mailto:" + target);
+            output.Content.SetContent(target);
+        }
+    }
+}
